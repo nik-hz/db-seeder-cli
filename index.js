2e479ef2ce6f01e168fb7f71c96fde3858fc21e1
nik <nik0001@protonmail.com>
2021-06-01 15:39:43 +0200

    cli working, db seeder working

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 #!/usr/bin/env node
 
-const dbSeeder = require('./dbseeder')
+const axios = require('axios')
 
-dbSeeder(process.argv[0], process.argv[1])
+const dbSeeder = async (seedDataLocation, apiUrl) => {
+    console.log('FIRST ARG - seed data location', seedDataLocation)
+    console.log('SECOND ARG - api url', apiUrl)
+
+    const seedData = require(seedDataLocation)
+    console.log(seedData)
+
+    try {
+        for (let item of seedData) {
+            const postreq = await axios.post(apiUrl, item)
+
+            console.log(postreq, 'succesfully sent')
+        }
+    } catch (error) {
+        console.log('')
+        console.log('')
+        console.log('THERE SEEMS TO BE A PROBLEM')
+        console.log('')
+        console.log('FIRST ARG - seed data location', seedDataLocation)
+        console.log('SECOND ARG - api url', apiUrl)
+        console.log('')
+        console.log('')
+        console.error(error.message)
+    }
+}
+
+dbSeeder(process.argv[2], process.argv[3])
